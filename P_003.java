8e4d831917dfbb0890b106fe90c11bf2d84c8016
f.p.khodaparast <f.p.khodaparast@gmail.com>
2016-10-03 23:49:49 +0330

    Add files via upload

---
+++ P_003.java
@@ -0,0 +1,22 @@
+package euler;
+
+/*
+ The prime factors of 13195 are 5, 7, 13 and 29.
+
+ What is the largest prime factor of the number 600851475143 ?
+ */
+public class P_003 {
+    public static void main(String[] args) {
+        Long n = 600851475143l;
+        for (int i = 1; i < n / 2; i++) {
+            if (n % i == 0) {
+                n = n / i;
+
+            }
+
+
+        }
+        System.out.println(n);
+    }
+}
+
